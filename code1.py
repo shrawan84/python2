d6092995d72b61d97ce8a48f32dee4c193e50231
shrawan <sshrawan1984@gmail.com>
2020-03-02 12:05:17 +0530

    python file

---
+++ code1.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+
+import time
+
+
+print("hello coder")
+time.sleep(2)
+
+print("hello google")
+time.sleep(2)
+
+
+print("hello twitter")
+time.sleep(2)
+
+
+print("hello facebook")
+time.sleep(2)
+
+print("hello Orkut")
+time.sleep(2)
